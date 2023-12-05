062b68c7244d333785595bb6972900bfcdfbbc84
mikha-dev <mikha.dev@gmail.com>
2023-12-05 19:53:16 +0300

    email dir

---
+++ src/Enums/EmailDirectionType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace D4T\Core\Enums;
+
+use D4T\UI\Enums\StyleClassType;
+use D4T\Core\Traits\D4TEnumTrait;
+use D4T\UI\Contracts\D4TEnumColored;
+
+enum EmailDirectionType: int implements D4TEnumColored
+{
+    use D4TEnumTrait;
+
+    case IN = 1;
+    case OUT = 2;
+    case REPLY = 3;
+
+    public function color(): StyleClassType
+    {
+        return match ($this) {
+            self::IN    => StyleClassType::PRIMARY,
+            self::OUT   => StyleClassType::SECONDARY,
+            self::REPLY => StyleClassType::INFO,
+            default     => StyleClassType::DARK
+        };
+    }
+}

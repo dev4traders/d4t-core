53da2e382fa957ad0dc9fa469de0a9f4078c0d27
mikha-dev <mikha.dev@gmail.com>
2023-12-12 22:18:03 +0300

    fix

--- src/Models/HelpCategory.php
+++ src/Models/HelpCategory.php
@@ -4,24 +4,19 @@ namespace D4T\Core\Models;
 
 use Illuminate\Support\Str;
 use D4T\Core\Models\HelpPost;
-use Dcat\Admin\Models\Domain;
 use D4T\Core\Models\HelpGroup;
 use Illuminate\Database\Eloquent\Model;
 use D4T\Core\Traits\HasDateTimeFormatter;
+use D4T\Core\Traits\HasDomain;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class HelpCategory extends Model
 {
     use HasDateTimeFormatter;
+    use HasDomain;
 
     protected $table = 'help_categories';
-
-    public function domain() : BelongsTo
-    {
-        return $this->belongsTo(Domain::class, 'domain_id');
-    }
 
     public function groups() : HasMany
     {
